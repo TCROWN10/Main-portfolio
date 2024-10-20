1d76409484e2b6d03d4ef3d7b6990db05f7cdcfa
Bolaji oluwatobi <bolajioluwatobi249@gmail.com>
2024-10-20 03:52:10 +0100

    worked on my button

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -20,7 +20,7 @@ const Banner = () => {
                         </div>
                         <p className='mb-8 max-w-lg mx-auto lg:mx-0'>I'm a motivated professional whose expertise is in HTML5,CSS3, Javascript, TailwindCSS, ReactJS and API's. Adept at troubleshooting and resolving complex technical issues, while consistently meeting deadlines and client expectations. </p>
                         <div className='flex gap-x-6 max-w-max mx-auto lg:mx-0 items-center mb-12'>
-                            <button className='btn btn-lag'>
+                            <button className='btn btn-sm'>
                                 <Link to="contact" smooth={true}>Contact me</Link>
                             </button>
                             <Link to="work" smooth={true} className="text-gradient btn-link cursor-pointer "> my Portfolio</Link>
